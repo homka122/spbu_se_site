2b1097a2411e62b7dff19b08d4cabd4ac5f58ce6
Homka122 <kombaev02@gmail.com>
2024-08-22 10:50:56 +0300

    refactor: rename 'summer_school' to 'summer_school_2021'

--- src/flask_se.py
+++ src/flask_se.py
@@ -653,9 +653,9 @@ def nooffer():
 
 
 @app.route("/summer_school_2021.html")
-def summer_school():
+def summer_school_2021():
     projects = SummerSchool.query.filter_by(year=2021).all()
-    return render_template("summer_school.html", projects=projects)
+    return render_template("summer_school_2021.html", projects=projects)
 
 
 @app.route("/summer_school_2022.html")
