cf6d6e4abec77105528ec5296ac75f5eca916ef7
Homka122 <kombaev02@gmail.com>
2024-08-22 12:03:30 +0300

    feat: replace summer schools pages to single page
    header now have only one option for summer school
    there is page with list of all existing summer schools now
    all summer school page have url to list of all summer schools

--- src/flask_se.py
+++ src/flask_se.py
@@ -670,6 +670,29 @@ def summer_school_2024():
     return render_template("summer_school_2024.html", projects=projects)
 
 
+@app.route("/summer_school_list.html")
+def summer_school_list():
+    schools = [
+        {
+            "name": "Летняя школа 2021",
+            "description": "Школа проводилась с <strong>12-го июля по 6-е августа</strong>. 12 июля в 12:00 прошло открытие школы и презентация проектов. 6 августа были проведены презентации результатов проектов, подведение итогов и закрытие школы.",
+            "href": "summer_school_2021",
+        },
+        {
+            "name": "Летняя школа 2022",
+            "description": "Школа проводилась с <strong>4 по 30 июля</strong>. 4 июля в 12:00 прошло открытие школы и презентация проектов. 30 июля были проведены презентации результатов проектов, подведение итогов и закрытие школы.",
+            "href": "summer_school_2022",
+        },
+        {
+            "name": "Летняя школа 2024",
+            "description": "Школа проводилась с <strong>8 июля по 3 августа</strong>. 8 июля в 12:00 прошло открытие школы и презентация проектов. 2 августа были проведены презентации результатов проектов, подведение итогов и закрытие школы.",
+            "href": "summer_school_2024",
+        },
+    ]
+
+    return render_template("summer_school_list.html", schools=schools)
+
+
 @app.route("/sitemap.xml", methods=["GET"])
 @app.route("/Sitemap.xml", methods=["GET"])
 def sitemap():
